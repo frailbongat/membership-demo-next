6e843cd208e28a08542402b3d282c0538c3b40a3
Frail Bongat <frail.bongat@gmail.com>
2021-05-24 10:39:01 +0800

    Feat: add serverside test

--- pages/serverside.js
+++ pages/serverside.js
@@ -1,39 +1,27 @@
+import Products from '../components/Products';
 import styles from '../styles/Home.module.css';
 
-const serverside = () => {
+const serverside = ({ products }) => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>getServerSideProps</h1>
 
-      <div className={styles.grid}>
-        <a href='https://nextjs.org/docs' className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href='https://nextjs.org/learn' className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href='https://github.com/vercel/next.js/tree/master/examples'
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
+      <Products products={products} />
     </main>
   );
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `https://membership-demo-strapi.herokuapp.com/products`
+  );
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+  };
 };
 
 export default serverside;
